e2fe272e46914c6c24762c13ace8871fab8e267b
guotonglin <tlin2011@163.com>
2016-05-11 10:22:44 +0800

    优化

---
+++ zewatch/Category/UILabel+CustomFont.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+CustomFont.h
+//  zewatch
+//
+//  Created by guotonglin on 16/5/6.
+//  Copyright © 2016年 mykronoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (CustomFont)
+
+@end
---
+++ zewatch/Category/UILabel+CustomFont.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+CustomFont.m
+//  zewatch
+//
+//  Created by guotonglin on 16/5/6.
+//  Copyright © 2016年 mykronoz. All rights reserved.
+//
+
+#import "UILabel+CustomFont.h"
+
+@implementation UILabel (CustomFont)
+
+@end
