008da7b33db27bf5f5ac1963bd8e15679e2d4fb4
guotonglin <tlin2011@163.com>
2016-05-11 14:50:22 +0800

    添加单元测试

--- zewatchTests/zewatchTests.m
+++ zewatchTests/zewatchTests.m
@@ -25,15 +25,9 @@
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+     XCTFail(@"No implementation for");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+
 
 @end
